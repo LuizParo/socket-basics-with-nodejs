acf8f5f177e151e7c15dbe2bd1163453b1205b99
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-02-21 21:46:06 -0300

    Setting up Socket.io

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 let app = require('./config/express');
 let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
+
+io.on('connection', () => console.log('User connected via socket.io'));
 
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`))
