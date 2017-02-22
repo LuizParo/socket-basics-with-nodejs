d72a2d7a9a98630ed2fb8da1e7d8e4994ae4ffc3
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-02-22 20:40:00 -0300

    Adding a custom message event

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -4,4 +4,8 @@
     let socket = io();
 
     socket.on('connect', () => console.log('Connected to socket.io server!'));
+
+    socket.on('message', message => console.log(message.text));
+
+    socket.emit('message', {text : 'Hello world'});
 }());
--- server.js
+++ server.js
@@ -4,6 +4,18 @@ let io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
-io.on('connection', () => console.log('User connected via socket.io'));
+io.on('connection', socket => {
+    console.log('User connected via socket.io');
+
+    socket.on('message', message => {
+        console.log(`Message received: ${message.text}`);
+        socket.broadcast.emit('message', message);
+    });
+
+    io.emit('message', {
+        text : 'Welcome to the chat application'
+    });
+});
+
 
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`))
