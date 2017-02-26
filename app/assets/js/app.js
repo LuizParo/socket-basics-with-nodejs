9ddcc4a1c0754323a0c54ca3f9a2625893fafd7f
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-02-26 11:49:01 -0300

    Adding the chat room name feature

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -2,17 +2,18 @@
     'use strict'
 
     let name = getQueryVariable('name') || 'Anonymous';
-    let room = getQueryVariable('room');
+    let room = getQueryVariable('room') || 'Default';
 
     let socket = io();
 
     socket.on('connect', () => console.log('Connected to socket.io server!'));
 
+    $('.room-title').text(room);
+
     socket.on('message', message => {
         console.log(message.text);
 
         let timestamp = moment.utc(message.timestamp).local().format('h:mm a');
-
         let $message = $('.messages');
         $message.append(`<p><strong>${message.name} ${timestamp}</strong></p>`);
         $message.append(`<p>${message.text}</p>`);
@@ -23,6 +24,8 @@
         event.preventDefault();
 
         let $message = $form.find('input[name=message]');
+
+        console.log(room);
 
         socket.emit('message', {
             name : name,
